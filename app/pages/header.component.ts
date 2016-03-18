3fd117850f0484570ca957d5cde771dd2c2e818f
fabiorecife <fabiorecife@gmail.com>
2016-03-18 18:12:02 -0300

    header component

---
+++ app/pages/header.component.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
+
+
+@Component({
+    selector: 'app-header',
+    templateUrl: 'app/pages/header.component.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+
+export class HeaderComponent {
+  title = "Admin";
+  subtitle = "JFN";
+}
