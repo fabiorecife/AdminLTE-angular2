a7676c339c4eb4a53e3c204854727c5acefe97ea
fabiorecife <fabiorecife@gmail.com>
2016-03-18 14:58:51 -0300

    footer component

--- app/pages/footer.component.ts
+++ app/pages/footer.component.ts
@@ -1,10 +1,18 @@
 import {Component,ElementRef, OnInit} from 'angular2/core';
+
+
 @Component({
     selector: 'app-footer',
     templateUrl: 'app/pages/footer.component.html'
+
 })
 
 export class FooterComponent {
-  
+  copyright = {
+    url: "http://fabioalmeida.net",
+    name: "Fabio Almeida",
+    year: "2016"
+  };
+  version = "0.1";
 
 }
