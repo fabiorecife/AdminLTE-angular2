40a6af8675e06dea82c8731ec44156388cff3b61
fabiorecife <fabiorecife@gmail.com>
2016-03-20 09:05:32 -0300

    add selectors to pages icons and buttons

--- app/pages/UI/buttons.component.ts
+++ app/pages/UI/buttons.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from 'angular2/core';
 
 @Component({
+    selector: 'pages-ui-buttons',
     templateUrl: 'app/pages/UI/buttons.component.html'
 })
 export class ButtonsComponent implements OnInit {
--- app/pages/UI/icons.component.ts
+++ app/pages/UI/icons.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from 'angular2/core';
 
 @Component({
+    selector: 'pages-ui-icons',
     templateUrl: 'app/pages/UI/icons.component.html'
 })
 export class IconsComponent implements OnInit {
