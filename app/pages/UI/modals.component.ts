09fb4572eb34592128101f03433b194d7aa682cc
fabiorecife <fabiorecife@gmail.com>
2016-03-25 05:00:37 -0300

    modals component

---
+++ app/pages/UI/modals.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+    templateUrl: 'app/pages/UI/modals.component.html'
+})
+export class ModalsComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
+}
