92ccbb49c675bb916a12c5ce8f0c0538e6e82db5
fabiorecife <fabiorecife@gmail.com>
2016-04-30 04:10:21 -0300

    login page

--- app/pages/examples/login.component.ts
+++ app/pages/examples/login.component.ts
@@ -1,12 +1,32 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component,ElementRef, OnInit} from 'angular2/core';
+declare var System;
+declare var $;
+declare var jQuery;
 
 @Component({
     selector: 'pages-examples-login',
     templateUrl: 'app/pages/examples/login.component.html'
+    /*
+    <!-- iCheck -->
+    <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+    */
 })
 export class LoginComponent implements OnInit {
-
+  elementRef: ElementRef;
+  constructor(elementRef: ElementRef) {
+        this.elementRef = elementRef;
+    }
   ngOnInit() {
+
       console.log("NG INIT - LoginComponent");
+      /*
+      System.import('plugins/iCheck/icheck.min');
+      setTimeout(() => $(this.elementRef.nativeElement).find('#rememberme').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+      }), 1000);
+      */
+
   }
 }
--- app/root.component.ts
+++ app/root.component.ts
@@ -2,17 +2,18 @@ import {Component} from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {AppComponent} from './app.component';
 
+import {LoginComponent} from './pages/examples/login.component';
+
 @Component({
     selector: 'app-root',
     template: `
-    <div class="hold-transition skin-blue sidebar-mini">
     <router-outlet></router-outlet>
-    <div>
     `,
     directives: [ROUTER_DIRECTIVES]
 })
 
 @RouteConfig([
+  { path: '/login',  name: 'Login',  component: LoginComponent },
   {
     path: '/home/...',
     name: 'Home',
